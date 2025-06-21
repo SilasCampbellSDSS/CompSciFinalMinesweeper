b353bf2501638eb03ea863b0edc660d3676fe505
SilasCampbellSDSS <159575993+SilasCampbellSDSS@users.noreply.github.com>
2025-06-20 21:22:07 -0400

    Update Bricks.cs

--- CompSciFinalMinesweeper/Bricks.cs
+++ CompSciFinalMinesweeper/Bricks.cs
@@ -9,17 +9,20 @@ namespace CompSciFinalMinesweeper
 {
     class Bricks
     {
+        //Values for organization when making bricks
         public static int rowsColumns = 19;
         public static int brickDimensions = 36;
         public static int spacing = 5;
         public static int startingSpacing = 10;
 
+        //More variables. Part of my problem was as simple as having nearMines on public static instead of public.
         public int x, y;
         public Rectangle rect;
         public int nearMines = 0;
         public bool isRevealed = false;
         public bool isFlagged = false;
 
+        //Creating the rectangles
         public Bricks(int x, int y, int size)
         {
             this.x = x;
