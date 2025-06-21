2f4216f310a6f81d7a05a86237bb556c0ab5a17c
SilasCampbellSDSS <159575993+SilasCampbellSDSS@users.noreply.github.com>
2025-06-21 16:25:46 -0400

    Update GameScreen.cs

--- CompSciFinalMinesweeper/GameScreen.cs
+++ CompSciFinalMinesweeper/GameScreen.cs
@@ -31,15 +31,11 @@ namespace CompSciFinalMinesweeper
 
         private void gameTimer_Tick(object sender, EventArgs e)
         {
-
-            mineNumber.Text = $"{mines.Count() - clickedFlag.Count()}";
+            mineNumber.Text = $"{mines.Count - clickedFlag.Count}";
             int cursorX = Cursor.Position.X;
             int cursorY = Cursor.Position.Y - 20;
-
             click = new Rectangle(cursorX, cursorY, 10, 10);
             Refresh();
-
-            
         }
 
         private void GameScreen_Click(object sender, EventArgs e)
@@ -50,57 +46,60 @@ namespace CompSciFinalMinesweeper
                 {
                     if (_flagSelect)
                     {
-                        if (bricks[i].isFlagged == false && bricks[i].isRevealed == false)
+                        if (!bricks[i].isFlagged && !bricks[i].isRevealed)
                         {
-                            clickedFlag.Add(bricks[i].rect);
+                            if (!clickedFlag.Contains(bricks[i].rect))
+                                clickedFlag.Add(bricks[i].rect);
+
                             bricks[i].isFlagged = true;
                         }
-
-                        if (bricks[i].isFlagged == true)
+                        else if (bricks[i].isFlagged)
                         {
+                            clickedFlag.Remove(bricks[i].rect);
                             bricks[i].isFlagged = false;
                         }
                     }
-                    else 
+                    else
                     {
-                        clickedBrick.Add(bricks[i].rect);
-                        bricks[i].isRevealed = true;
-
+                        if (!bricks[i].isFlagged)
+                        {
+                            clickedBrick.Add(bricks[i].rect);
+                            bricks[i].isRevealed = true;
+                        }
                     }
                 }
             }
 
-            for (int i = 0; i < mines.Count(); i++)
+            for (int i = 0; i < mines.Count; i++)
             {
                 if (click.IntersectsWith(mines[i].mineRect))
                 {
                     if (_flagSelect)
                     {
-                        if (mines[i].isFlagged == false)
+                        if (!mines[i].isFlagged)
                         {
-                            clickedFlag.Add(mines[i].mineRect);
-                            minesClicked++;
+                            if (!clickedFlag.Contains(mines[i].mineRect))
+                                clickedFlag.Add(mines[i].mineRect);
+
                             mines[i].isFlagged = true;
+                            minesClicked++;
                         }
                         else
                         {
-                            for(int j = 0; j < clickedFlag.Count(); j++)
-                            {
-                                clickedFlag.RemoveAt(j);
-                            }
-
+                            clickedFlag.Remove(mines[i].mineRect);
                             mines[i].isFlagged = false;
                             minesClicked--;
                         }
 
-                        if (minesClicked == mines.Count())
+                        bool allCorrect = mines.All(m => m.isFlagged);
+                        if (allCorrect)
                         {
                             loss = false;
                             GameOver();
                             return;
                         }
                     }
-                    else
+                    else if (!mines[i].isFlagged)
                     {
                         loss = true;
                         GameOver();
@@ -138,15 +137,15 @@ namespace CompSciFinalMinesweeper
             {
                 Rectangle[] orbitals =
                 {
-                    new Rectangle(brick.x + 42, brick.y, 10, 10),
-                    new Rectangle(brick.x - 42, brick.y, 10, 10),
-                    new Rectangle(brick.x, brick.y + 42, 10, 10),
-                    new Rectangle(brick.x, brick.y - 42, 10, 10),
-                    new Rectangle(brick.x + 42, brick.y + 42, 10, 10),
-                    new Rectangle(brick.x - 42, brick.y + 42, 10, 10),
-                    new Rectangle(brick.x + 42, brick.y - 42, 10, 10),
-                    new Rectangle(brick.x - 42, brick.y - 42, 10, 10),
-                };
+                new Rectangle(brick.x + 42, brick.y, 10, 10),
+                new Rectangle(brick.x - 42, brick.y, 10, 10),
+                new Rectangle(brick.x, brick.y + 42, 10, 10),
+                new Rectangle(brick.x, brick.y - 42, 10, 10),
+                new Rectangle(brick.x + 42, brick.y + 42, 10, 10),
+                new Rectangle(brick.x - 42, brick.y + 42, 10, 10),
+                new Rectangle(brick.x + 42, brick.y - 42, 10, 10),
+                new Rectangle(brick.x - 42, brick.y - 42, 10, 10),
+            };
 
                 foreach (var orbital in orbitals)
                 {
@@ -161,16 +160,9 @@ namespace CompSciFinalMinesweeper
         public void GameOver()
         {
             instructionsLabel.Visible = true;
-
-            if (loss == true)
-            {
-                instructionsLabel.Text = "YOU LOST, PRESS + TO PLAY AGAIN";
-            }
-            else
-            {
-                instructionsLabel.Text = "YOU WON, PRESS + TO PLAY AGAIN";
-            }
-            
+            instructionsLabel.Text = loss
+                ? "YOU LOST, PRESS + TO PLAY AGAIN"
+                : "YOU WON, PRESS + TO PLAY AGAIN";
         }
 
         private Image DisplayNearMines(int nearMines)
@@ -194,7 +186,12 @@ namespace CompSciFinalMinesweeper
         {
             foreach (var brick in bricks)
             {
-                if (loss == false)
+                if (loss)
+                {
+                    e.Graphics.DrawImage(Properties.Resources.openedMine, brick.rect);
+                }
+                else
+                {
                     if (brick.isRevealed)
                     {
                         e.Graphics.DrawImage(DisplayNearMines(brick.nearMines), brick.rect);
@@ -207,32 +204,17 @@ namespace CompSciFinalMinesweeper
                     {
                         e.Graphics.DrawImage(Properties.Resources.unopenedMine, brick.rect);
                     }
-                else
-                {
-                    e.Graphics.DrawImage(Properties.Resources.openedMine, brick.rect);
                 }
-                
             }
 
             foreach (var mine in mines)
             {
-                if (loss == false)
-                    e.Graphics.DrawImage(Properties.Resources.unopenedMine, mine.mineRect);
-                else
-                {
-                    e.Graphics.DrawImage(Properties.Resources.mine, mine.mineRect);
-                }
+                e.Graphics.DrawImage(loss ? Properties.Resources.mine : Properties.Resources.unopenedMine, mine.mineRect);
             }
 
             foreach (var flag in clickedFlag)
             {
-                if (loss == false)
-                     e.Graphics.DrawImage(Properties.Resources.flaggedMine, flag);
-                else
-                {
-                    e.Graphics.DrawImage(Properties.Resources.openedMine, flag);
-                }
-
+                e.Graphics.DrawImage(loss ? Properties.Resources.openedMine : Properties.Resources.flaggedMine, flag);
             }
 
             e.Graphics.FillRectangle(brush, click);
@@ -249,13 +231,15 @@ namespace CompSciFinalMinesweeper
             mines.Clear();
             clickedBrick.Clear();
             clickedFlag.Clear();
+            minesClicked = 0;
+            loss = false;
+
             CreateBricks();
             CalculateNearMines();
             Refresh();
-            instructionsLabel.Text = "- Click on flag icon to toggle to flagging items, click to switch back\r\n- Click on plus icon to restart game\r\n- Click ESC key to leave game\r\n- When a square is uncovered and does not display a number that means that there are no mines nearby\r\n- Click on this label to close it :)\r\n";
-            instructionsLabel.Visible = false;
 
-            loss = false;
+            instructionsLabel.Text = "- Click on flag icon to toggle to flagging items, click to switch back\r\n- Click on plus icon to restart game\r\n- Click ESC key to leave game\r\n- When a square is uncovered and does not display a number that means that there are no mines nearby\r\n- Click on this label to close it :)";
+            instructionsLabel.Visible = false;
         }
 
         private void infoSelect_Click(object sender, EventArgs e)
@@ -265,21 +249,16 @@ namespace CompSciFinalMinesweeper
 
         private void instructionsLabel_Click(object sender, EventArgs e)
         {
-            if (instructionsLabel.Visible == true)
-            {
-                instructionsLabel.Visible = false;
-            }
+            instructionsLabel.Visible = false;
         }
 
         private void GameScreen_KeyDown(object sender, KeyEventArgs e)
         {
-            switch (e.KeyCode)
+            if (e.KeyCode == Keys.Escape)
             {
-                case Keys.Escape:
-                    Application.Exit();
-                    break;
+                Application.Exit();
             }
-            
         }
     }
+
 }
