5dd28ca67c146da6164b29c45db14695de4a8493
SilasCampbellSDSS <159575993+SilasCampbellSDSS@users.noreply.github.com>
2025-06-20 22:50:11 -0400

    Update GameScreen.cs

--- CompSciFinalMinesweeper/GameScreen.cs
+++ CompSciFinalMinesweeper/GameScreen.cs
@@ -9,17 +9,14 @@ namespace CompSciFinalMinesweeper
 {
     public partial class GameScreen : UserControl
     {
-    //Lists for bricks, mines and our flags (no class for flags, just rectangles)
         List<Bricks> bricks = new List<Bricks>();
         List<Mines> mines = new List<Mines>();
         List<Rectangle> clickedBrick = new List<Rectangle>();
         List<Rectangle> clickedFlag = new List<Rectangle>();
 
-    //Rectangle to see precisely where you're clicking
         SolidBrush brush = new SolidBrush(Color.Black);
         Rectangle click = new Rectangle(0, 0, 10, 10);
 
-    //Random generator for mines and bool variables
         Random randomGen = new Random();
         bool _flagSelect = false;
         bool loss = false;
@@ -34,14 +31,11 @@ namespace CompSciFinalMinesweeper
 
         private void gameTimer_Tick(object sender, EventArgs e)
         {
-        //How many mines remain
-            mineNumber.Text = $"{mines.Count() - clickedFlag.Count()}";
 
-        //Getting cursor position
+            mineNumber.Text = $"{mines.Count() - clickedFlag.Count()}";
             int cursorX = Cursor.Position.X;
             int cursorY = Cursor.Position.Y - 20;
 
-        //Placing rectangle at cursor coordinates
             click = new Rectangle(cursorX, cursorY, 10, 10);
             Refresh();
 
@@ -50,21 +44,25 @@ namespace CompSciFinalMinesweeper
 
         private void GameScreen_Click(object sender, EventArgs e)
         {
-
-            //If you click a brick
             for (int i = 0; i < bricks.Count; i++)
             {
                 if (click.IntersectsWith(bricks[i].rect))
                 {
                     if (_flagSelect)
                     {
-                    //If you have flag toggled on flag it, add it to the list and keep track of it
-                        clickedFlag.Add(bricks[i].rect);
-                        bricks[i].isFlagged = true;
+                        if (bricks[i].isFlagged == false && bricks[i].isRevealed == false)
+                        {
+                            clickedFlag.Add(bricks[i].rect);
+                            bricks[i].isFlagged = true;
+                        }
+
+                        if (bricks[i].isFlagged == true)
+                        {
+                            bricks[i].isFlagged = false;
+                        }
                     }
-                    else
+                    else 
                     {
-                    //If you don't have flag toggled on, add the brick to a seperate list and keep track of it
                         clickedBrick.Add(bricks[i].rect);
                         bricks[i].isRevealed = true;
 
@@ -72,15 +70,28 @@ namespace CompSciFinalMinesweeper
                 }
             }
 
-            foreach (var mine in mines)
+            for (int i = 0; i < mines.Count(); i++)
             {
-                if (click.IntersectsWith(mine.mineRect))
+                if (click.IntersectsWith(mines[i].mineRect))
                 {
                     if (_flagSelect)
                     {
-                    //Same case as bricks
-                        clickedFlag.Add(mine.mineRect);
-                        minesClicked++;
+                        if (mines[i].isFlagged == false)
+                        {
+                            clickedFlag.Add(mines[i].mineRect);
+                            minesClicked++;
+                            mines[i].isFlagged = true;
+                        }
+                        else
+                        {
+                            for(int j = 0; j < clickedFlag.Count(); j++)
+                            {
+                                clickedFlag.RemoveAt(j);
+                            }
+
+                            mines[i].isFlagged = false;
+                            minesClicked--;
+                        }
 
                         if (minesClicked == mines.Count())
                         {
@@ -91,11 +102,7 @@ namespace CompSciFinalMinesweeper
                     }
                     else
                     {
-
-                    //If you click on a mine when you don't have flag toggled on then call Game Over
-
                         loss = true;
-
                         GameOver();
                         return;
                     }
@@ -105,17 +112,14 @@ namespace CompSciFinalMinesweeper
 
         public void CreateBricks()
         {
-        //Create a column of bricks, then create rows of each of those so that the whole board is covered
             for (int i = 0; i < Bricks.rowsColumns; i++)
             {
                 for (int j = 0; j < Bricks.rowsColumns; j++)
                 {
-                //Calculations for where to place bricks
                     int x = j * (Bricks.brickDimensions + Bricks.spacing) + Bricks.startingSpacing;
                     int y = i * (Bricks.brickDimensions + Bricks.spacing) + 53;
                     int mineChance = randomGen.Next(1, 21);
 
-                //Making 15% mines, rest bricks
                     if (mineChance < 4)
                     {
                         mines.Add(new Mines(x, y, Mines.mineDimensions));
@@ -132,10 +136,6 @@ namespace CompSciFinalMinesweeper
         {
             foreach (var brick in bricks)
             {
-            //Array of rectangles orbiting the main rectangle (cursor). 
-            //I use these to intercect with bricks around the one the player is clicking on, 
-            //then I can keep track of where mines are and update visuals accordingly
-            
                 Rectangle[] orbitals =
                 {
                     new Rectangle(brick.x + 42, brick.y, 10, 10),
@@ -150,9 +150,6 @@ namespace CompSciFinalMinesweeper
 
                 foreach (var orbital in orbitals)
                 {
-                //If these orbitals intercect with a mine then tally that off on a non static variable that applies to all bricks 
-                //This took me forever to figure out how to do properly 
-                
                     if (mines.Any(m => orbital.IntersectsWith(m.mineRect)))
                     {
                         brick.nearMines++;
@@ -163,10 +160,6 @@ namespace CompSciFinalMinesweeper
 
         public void GameOver()
         {
-        //Update bool variable
-            loss = true;
-
-        //Display message
             instructionsLabel.Visible = true;
 
             if (loss == true)
@@ -184,8 +177,6 @@ namespace CompSciFinalMinesweeper
         {
             switch (nearMines)
             {
-            //Creating a method that grabs how many mines are near and display the appropriate image
-            //Learned how to do this from Alistair in the brick breaker project so shout out to him
                 case 0: return Properties.Resources.openedMine;
                 case 1: return Properties.Resources._1near;
                 case 2: return Properties.Resources._2near;
@@ -203,7 +194,6 @@ namespace CompSciFinalMinesweeper
         {
             foreach (var brick in bricks)
             {
-            //If the game is active display bricks accordingly
                 if (loss == false)
                     if (brick.isRevealed)
                     {
@@ -217,7 +207,6 @@ namespace CompSciFinalMinesweeper
                     {
                         e.Graphics.DrawImage(Properties.Resources.unopenedMine, brick.rect);
                     }
-                //If the game is over make all non mines transparent 
                 else
                 {
                     e.Graphics.DrawImage(Properties.Resources.openedMine, brick.rect);
@@ -227,7 +216,6 @@ namespace CompSciFinalMinesweeper
 
             foreach (var mine in mines)
             {
-            //Same as above except that when you lose show where the mines are
                 if (loss == false)
                     e.Graphics.DrawImage(Properties.Resources.unopenedMine, mine.mineRect);
                 else
@@ -238,7 +226,6 @@ namespace CompSciFinalMinesweeper
 
             foreach (var flag in clickedFlag)
             {
-            //Same code but with flags
                 if (loss == false)
                      e.Graphics.DrawImage(Properties.Resources.flaggedMine, flag);
                 else
@@ -248,19 +235,16 @@ namespace CompSciFinalMinesweeper
 
             }
 
-        //Fill in the cursor rectangle
             e.Graphics.FillRectangle(brush, click);
         }
 
         private void flagSelect_Click(object sender, EventArgs e)
         {
-        //Toggling flagging on and off
             _flagSelect = !_flagSelect;
         }
 
         private void newGame_Click(object sender, EventArgs e)
         {
-        //Creating new values and resetting messages
             bricks.Clear();
             mines.Clear();
             clickedBrick.Clear();
@@ -268,11 +252,10 @@ namespace CompSciFinalMinesweeper
             CreateBricks();
             CalculateNearMines();
             Refresh();
-
             instructionsLabel.Text = "- Click on flag icon to toggle to flagging items, click to switch back\r\n- Click on plus icon to restart game\r\n- Click ESC key to leave game\r\n- When a square is uncovered and does not display a number that means that there are no mines nearby\r\n- Click on this label to close it :)\r\n";
             instructionsLabel.Visible = false;
-            loss = false;
 
+            loss = false;
         }
 
         private void infoSelect_Click(object sender, EventArgs e)
@@ -290,8 +273,6 @@ namespace CompSciFinalMinesweeper
 
         private void GameScreen_KeyDown(object sender, KeyEventArgs e)
         {
-        //Letting you get out of the game
-        
             switch (e.KeyCode)
             {
                 case Keys.Escape:
